fb4a2eb8db9b3eec32fe78308b6d928a439daca2
AethoceSora <lijinghong0233@gmail.com>
2024-08-02 11:25:57 +0800

    update:Optimize UI

--- src/pages/Directions.jsx
+++ src/pages/Directions.jsx
@@ -110,6 +110,6 @@ function PageImage({className}) {
     src="/assets/banner-raw.png"
     alt="logo"
     // className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-    className={`object-cover max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 w-[2432px] h-[1442px] ${className}`}
+    className={`object-cover max-w-none object-left-top rounded-xl shadow-xl ring-1 ring-gray-400/10 w-[2432px] h-[1442px] ${className}`}
   />)
 }
