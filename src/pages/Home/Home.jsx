c56d9f898b202b458be335e274d924ad10e65ecf
7890qwaszx <wyz@outlook.in>
2024-08-04 21:50:55 +0800

    update: 登录修改添加 submit 参数

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -95,6 +95,8 @@ export default function Home() {
       return
     }
 
+    formData.set('submit', '登录')
+
     request({
       url: '/api/login',
       method: 'POST',
@@ -169,7 +171,7 @@ export default function Home() {
           className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <img
             className="relative block dark:drop-shadow-[0_0_0.3rem_#ffffff70] w-[180px] z-10"
-            src="/assets/qlu-logo-solid.png"
+            src="/assets/qlu-logo-solid-compressed.png"
             alt="齐鲁工大（鲁科院）Logo"
           />
           {/*<img*/}
