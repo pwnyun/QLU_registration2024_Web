7bad83b1b6a063de1eeb76541ca7d4e5119599cd
7890qwaszx <wyz@outlook.in>
2024-08-04 17:07:24 +0800

    update: Vite SRI

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sri from "@small-tech/vite-plugin-sri";
 
 // https://vitejs.dev/config/
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),sri()],
 })
