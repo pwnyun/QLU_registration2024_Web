a84949eeb15b0e754cf86291b7d7f27dfc05d23a
7890qwaszx <wyz@outlook.in>
2024-08-21 19:19:38 +0800

    bugfix:

--- src/pages/SAMBind.jsx
+++ src/pages/SAMBind.jsx
@@ -135,10 +135,7 @@ export default function SAMBind() {
           </div>
         </div>
 
-        <form ref={formRef} onSubmit={(e) => {
-          e.preventDefault();
-          submit();
-        }}>
+        <form ref={formRef} onSubmit={(e) => e.preventDefault()}>
 
           <div className="border-b border-gray-900/10 p-4 pb-12">
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
@@ -221,6 +218,7 @@ export default function SAMBind() {
                 <button
                   type="submit"
                   className="rounded-md bg-qlu w-full md:w-32 md:rounded-full px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={submit}
                 >
                   提交
                 </button>
